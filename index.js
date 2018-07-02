e842593f58d1604bc69fa7f638d661773ea0b477
yomnabarake21 <yomnabarakate21@gmail.com>
2018-07-02 10:11:19 +0200

    Added routes files

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const express = require ('express');
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser =requre('body-parser');
+const bodyParser =require('body-parser');
 const app = express();
+const fs = require('file-system');
+
+//load all your routes dynamically
+fs.readdirSync('./Controllers').forEach(function(file){
+    var routes = require('./Controllers/'+file);
+    routes (app);
+    console.log(file);
+
+});
+
+//specify local host port on which the app is running.
+const PORT = 8000;
+
+app.listen(PORT,()=>{
+    console.log(`We are listening on port ${PORT}`) ;
+})
