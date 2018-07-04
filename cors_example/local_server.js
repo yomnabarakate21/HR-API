9cf9ce1e356d0c60c87e62aa9fbc51b2f157b1cd
Bassent <bassentzaghloul@gmail.com>
2018-07-04 12:15:00 +0000

    requiring cors module on a local server to grant upcoming requests

---
+++ cors_example/local_server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+cors = require('./CV_extractor_PJ/cors');
+
+
+app.use(express.static('public'));
+app.use(cors());
+app.get('/', function (req, res) {
+
+ var obj = require("/home/bassent/Desktop/fill_first_page.json");
+ res.send(obj);
+ console.log(obj)
+})
+
+
+var server = app.listen(8080, function () {
+var host = server.address().address
+var port = server.address().port
+  
+  
+ console.log("Example app listening at http://%s:%s", host, port);
+})
